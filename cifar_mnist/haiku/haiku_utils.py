a8c3fcdc4ec4a7c21cbfc2d772cd4f4ff4a85d4c
Charlie Snell <sea_snell@icloud.com>
2022-07-12 01:34:52 +0000

    haiku util

--- cifar_mnist/haiku/haiku_utils.py
+++ cifar_mnist/haiku/haiku_utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Iterator
+from typing import Any, Iterator, Callable, Optional
 import jax
 import numpy as np
 import collections
@@ -18,10 +18,12 @@ def split_rng_pytree(rng_pytree: Any, splits: int=2):
     split_rngs = jax.tree_util.tree_map(lambda x: tuple(jax.random.split(x, splits)), rng_pytree)
     return jax.tree_util.tree_transpose(outer_tree_def, jax.tree_util.tree_structure(tuple([0 for _ in range(splits)])), split_rngs)
 
-def batch_iterator(rng: jax.random.KeyArray, dataset: Any, bsize: int) -> Iterator:
+def batch_iterator(rng: jax.random.KeyArray, dataset: Any, bsize: int, postproc_f: Optional[Callable]=None) -> Iterator:
+    if postproc_f is None:
+        postproc_f = lambda x: x
     batches = batch_idxs(rng, len(dataset), bsize)
     for idxs in batches:
-        yield dataset[idxs]
+        yield postproc_f(dataset[idxs])
 
 def prefetch(iterator: Iterator, queue_size: int = 2) -> Iterator:
     # See https://flax.readthedocs.io/en/latest/_modules/flax/jax_utils.html#prefetch_to_device
