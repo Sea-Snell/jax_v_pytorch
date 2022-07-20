171236cb019c8f2868d67f18a4055bce117d61d6
Charlie Snell <sea_snell@icloud.com>
2022-07-20 20:32:51 +0000

    finetune

--- large_lm_finetune/flax/train_loop.py
+++ large_lm_finetune/flax/train_loop.py
@@ -232,43 +232,54 @@ class TrainLoop(ConfigScript):
         else:
             raise NotImplementedError
 
-        def get_initial_state2(params):
-            params = with_sharding_constraint(params, param_spec)
-            opt_state, params = get_initial_state(params)
-            return opt_state, params
-
         # pjit the get_initial_state function to shard params and init
         # optimizer state in sharded way
         if self.pjit:
             p_get_initial_state = pjit(
-                get_initial_state2,
+                get_initial_state, 
                 in_axis_resources=(param_spec,), 
                 out_axis_resources=(opt_state_spec, param_spec),
             )
         else:
             p_get_initial_state = get_initial_state
         
-        # def get_param_shapes(rng, input_shape):
-        #     return jax.eval_shape(model.init_weights, rng, input_shape)
+        # utils for splitting params per-host
+        def get_param_shapes(rng):
+            return model.init_weights(rng, (1, 1,))
         
-        # p_get_initial_state = pjit(
-        #     get_param_shapes,
-        #     in_axis_resources=(None,), 
-        #     out_axis_resources=param_spec, 
-        #     static_argnums=(1,), 
-        # )
+        def host_param_shard(host_param_shapes, params):
+            def split_param(host_shape, param):
+                param_shape_arr = jnp.array(param.shape, dtype=jnp.int32)
+                host_shape_arr = jnp.array(host_shape, dtype=jnp.int32)
+                mask = (param_shape_arr != host_shape_arr).astype(jnp.int32)
+                return jax.lax.dynamic_slice(param, mask * host_shape_arr * jax.process_index(), host_shape_arr)
+            return jax.tree_util.tree_map(split_param, host_param_shapes, params)
+        
+        if self.pjit:
+            p_get_param_shapes = pjit(
+                get_param_shapes,
+                in_axis_resources=(None,), 
+                out_axis_resources=param_spec, 
+            )
+        else:
+            p_get_param_shapes = get_param_shapes
 
         # mesh definition
         mesh_devices = np.array(jax.devices()).reshape(1, jax.device_count())
         print('using mesh shape:', mesh_devices.shape)
         print('full mesh:', mesh_devices)
 
+        # split the parameters per-host
+        with Mesh(mesh_devices, ("dp", "mp")):
+            rng, new_rng = jax.random.split(rng)
+            host_param_shapes = jax.eval_shape(p_get_param_shapes, new_rng)
+        with jax.default_device(jax.devices('cpu')[0]):
+            params = host_param_shard(host_param_shapes, params)
+
         # split the opt_state and params between all devices
         with Mesh(mesh_devices, ("dp", "mp")):
             opt_state, params = p_get_initial_state(params)
-            # rng, new_rng = jax.random.split(rng)
-            # params = p_get_initial_state(new_rng, (1, 1,))
-        # print(jax.tree_util.tree_map(lambda x: list(map(lambda y: y.shape, x.device_buffers)), params))
+        print(jax.tree_util.tree_map(lambda x: list(map(lambda y: y.shape, x.device_buffers)), params))
         print(jax.tree_util.tree_map(lambda x: x.shape, params))
         
         # # define lm training step
