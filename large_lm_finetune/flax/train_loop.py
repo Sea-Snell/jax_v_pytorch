5dc23cc9b56469d9699a142e456878a4920ce261
Charlie Snell <sea_snell@icloud.com>
2022-07-21 19:29:36 +0000

    finetune

--- large_lm_finetune/flax/train_loop.py
+++ large_lm_finetune/flax/train_loop.py
@@ -307,9 +307,6 @@ class TrainLoop(ConfigScript):
 
         # mesh definition
         mesh_devices = np.array(jax.devices()).reshape(1, jax.device_count())
-        temp1 = mesh_devices[:, 0:8].copy()
-        temp2 = mesh_devices[:, 24:32].copy()
-        mesh_devices[:, 0:8], mesh_devices[:, 24:32] = temp2, temp1
         print('using mesh shape:', mesh_devices.shape)
         print('full mesh:', mesh_devices)
 
